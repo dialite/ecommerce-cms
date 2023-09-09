208943b79cc78b7d458fda00606aadebb6fc2d05
dialite <raymondolisa01@gmail.com>
2023-09-09 18:16:16 +0100

    Install, initialize and create zod resolver for form schema type

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,30 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
 
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
 export const StoreModal = () => {
   const StoreModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    //TODO: Create Store
+  };
 
   return (
     <Modal
